4c6f7a3a18345c31cead678e50d7d53b454daa32
Mohamed Nadir Bakadour <mohamed-nadir.bakadour@etu.mines-paristech.fr>
2022-10-26 15:24:10 +0200

    snake avec mvt sans fruit

--- snake.py
+++ snake.py
@@ -5,19 +5,52 @@ from random import randint
 import pygame as pg
 
 pg.init()
-screen = pg.display.set_mode((400, 300))
+screen = pg.display.set_mode((600, 600))
 clock = pg.time.Clock()
 
 # on rajoute une condition à la boucle: si on la passe à False le programme s'arrête
 running = True
+
+direction = (1, 0)
+fruit = (10, 10)
+
+def col_pos(a,color = (255, 0, 0)):
+    for c in a:
+        x = 20*c[0] # coordonnée x (colonnes) en pixels
+        y = 20*c[1] # coordonnée y (lignes) en pixels
+        width = 20 # largeur du rectangle en pixels
+        height = 20 # hauteur du rectangle en pixels
+        rect = pg.Rect(x, y, width, height)
+        pg.draw.rect(screen, color, rect)
+
+def move(sn,dir):
+    head=(sn[0][0] + dir[0], sn[0][1] + dir[1])
+    sn=[head]+sn[:-1]
+    return sn
+
+snake = [
+    (10, 15),
+    (11, 15),
+    (12, 15),]
+
 while running:
+    screen.fill((255,255,255))
+    for i in range(30):
+        for j in range(30):
+            if (i+j)%2==1:
+                x = 20*i # coordonnée x (colonnes) en pixels
+                y = 20*j # coordonnée y (lignes) en pixels
+                width = 20 # largeur du rectangle en pixels
+                height = 20 # hauteur du rectangle en pixels
+                rect = pg.Rect(x, y, width, height)
+                # appel à la méthode draw.rect()
+                color = (0, 0, 0)
+                pg.draw.rect(screen, color, rect)
+    
+    col_pos(snake)
 
-    clock.tick(1)
-
-    random_color = (randint(0, 255), randint(0, 255), randint(0, 255))
-    screen.fill(random_color)
     pg.display.update()
-
+    clock.tick(5)
     # on itère sur tous les évênements qui ont eu lieu depuis le précédent appel
     # ici donc tous les évènements survenus durant la seconde précédente
     for event in pg.event.get():
@@ -30,8 +63,15 @@ while running:
             # si la touche est "Q" on veut quitter le programme
             if event.key == pg.K_q:
                 running = False
-
-
+            if event.key == pg.K_UP and direction!=(0,1):
+                direction= (0,-1)
+            if event.key == pg.K_DOWN and direction!=(0,-1):
+                direction= (0,1)
+            if event.key == pg.K_LEFT and direction!=(1,0):
+                direction= (-1,0)
+            if event.key == pg.K_RIGHT and direction!=(1,0):
+                direction= (1,0)
+    snake=move(snake,direction)
 # Enfin on rajoute un appel à pg.quit()
 # Cet appel va permettre à Pygame de "bien s'éteindre" et éviter des bugs sous Windows
 pg.quit()
