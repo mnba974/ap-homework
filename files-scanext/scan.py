b90465a926f9f39e732ec8e108d06ec44f192a2b
Mohamed Nadir Bakadour <mohamed-nadir.bakadour@etu.mines-paristech.fr>
2022-11-07 19:04:41 +0100

    foction scan

---
+++ files-scanext/scan.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from datetime import datetime as DateTime
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('chaine')
+parser.add_argument('ext')
+args = parser.parse_args()
+chaine=args.chaine
+truc=args.ext
+
+
+def func(chaine,truc):
+    p=Path(chaine)
+    p.resolve()
+    for t in p.glob('**/*.'+truc):
+        t=t.resolve()
+        print(t)
+        time=DateTime.fromtimestamp(t.stat().st_mtime)
+        print(t.stat().st_size, time)
+        with open(t,'r') as f:
+            print(f.readline())
+func('.','py')
+
+
+func(chaine,truc)
